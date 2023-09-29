bae898c0038ae1e87cc4eb4b730aebfadf7db986
Emilie Lewe <emylewe@gmail.com>
2023-09-29 13:33:08 +0100

    Découvrir pourquoi presque tout a une valeur

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,7 +1,19 @@
 
 fun main(args: Array<String>) {
+    // 1. EXPLORE THE MAIN FUNCTION
  //  println("Hello, world!")
-    println("Hello, ${args[0]}")
+ //   println("Hello, ${args[0]}")
+
+    // 2. LEARN WHY EVERYTHING HAS A VALUE
+
+    //   val isUnit = println("This is an expression")
+    //   println(isUnit)
+    //   val temperature = 10
+    //   val isHot = if (temperature > 50) true else false
+    //   println(isHot)
+    val temperature = 10
+    val message = "The water temperature is ${ if( temperature > 50) "too warm" else "OK"}."
+    println(message)
 }
 
 
