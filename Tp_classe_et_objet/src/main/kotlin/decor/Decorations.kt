4dbe386a11ab0b53ccbc206c6aa762cd6161f679
Mike_Emilie <mike.j.ngan@gmail.com>
2023-10-04 13:27:44 +0100

    singleton et enumérateur

--- Tp_classe_et_objet/src/main/kotlin/decor/Decorations.kt
+++ Tp_classe_et_objet/src/main/kotlin/decor/Decorations.kt
@@ -7,3 +7,11 @@ data class Decorations (val rocks: String){
 data class Decoration2(val rocks: String, val wood: String, val diver: String){
     // cette classe de données prend directement les paramettres que kotlin va utiliser en créant directement pour chaque vaentrée une variable.
 }
+
+enum class Color(val rgb: Int) {
+    // chaque élément dans cette classe est un objet.
+    ROUGE(0xFF0000), VERT(0x00FF00), BLEU(0x0000FF);
+}
+enum class Direction(val degrees: Int) {
+    NORD(0), SUD(180), EST(90), OUEST(270)
+}
--- Tp_classe_et_objet/src/main/kotlin/decor/Main.kt
+++ Tp_classe_et_objet/src/main/kotlin/decor/Main.kt
@@ -2,4 +2,10 @@ package example.myapp.decor
 
 fun main(){
     makeDecorations()
+    // comme les singletons, il n'y a qu'une seule valeur pour chaque objet de la classe.
+    println(Direction.EST.name)
+    println(Color.ROUGE.name)
+
+    println(Direction.NORD.name)
+    println(Color.BLEU.name)
 }
