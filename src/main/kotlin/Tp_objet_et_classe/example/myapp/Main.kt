9f5925b67e286b3881625f94cfde3dd23dd89699
Emilie Lewe <emylewe@gmail.com>
2023-10-04 16:30:19 +0100

    Ajout d'un nouveau getter de propriété

--- src/main/kotlin/Tp_objet_et_classe/example/myapp/Main.kt
+++ src/main/kotlin/Tp_objet_et_classe/example/myapp/Main.kt
@@ -6,14 +6,18 @@ fun buildAquarium() {
     //myAquarium.height = 60
     //myAquarium.printSize()
 
-    val aquarium1 = Aquarium()
-    aquarium1.printSize()
-    val aquarium2 = Aquarium(width = 25)
-    aquarium2.printSize()
-    val aquarium3 = Aquarium(height = 35, length = 110)
-    aquarium3.printSize()
-    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
-    aquarium4.printSize()
+    //val aquarium1 = Aquarium()
+    //aquarium1.printSize()
+    //val aquarium2 = Aquarium(width = 25)
+    //aquarium2.printSize()
+    //val aquarium3 = Aquarium(height = 35, length = 110)
+    //aquarium3.printSize()
+    //val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    //aquarium4.printSize()
+
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    //println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
 
 }
 
