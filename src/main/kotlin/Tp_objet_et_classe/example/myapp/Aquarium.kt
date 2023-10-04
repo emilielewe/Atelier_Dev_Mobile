bc27b229e6cd77a7f1e73b3d55bd28712be52079
Emilie Lewe <emylewe@gmail.com>
2023-10-04 16:37:41 +0100

    Ajout d'un definisseur de propriétés

--- src/main/kotlin/Tp_objet_et_classe/example/myapp/Aquarium.kt
+++ src/main/kotlin/Tp_objet_et_classe/example/myapp/Aquarium.kt
@@ -23,8 +23,12 @@ class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40
         val tank = numberOfFish * 2000 * 1.1
         height = (tank / (length * width)).toInt()
     }
-    val volume: Int
+    var volume: Int
         get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
--- src/main/kotlin/Tp_objet_et_classe/example/myapp/Main.kt
+++ src/main/kotlin/Tp_objet_et_classe/example/myapp/Main.kt
@@ -17,6 +17,8 @@ fun buildAquarium() {
 
     val aquarium6 = Aquarium(numberOfFish = 29)
     aquarium6.printSize()
+    aquarium6.volume = 70
+    aquarium6.printSize()
     //println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
 
 }
