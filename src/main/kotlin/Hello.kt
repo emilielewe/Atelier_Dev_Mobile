4dcdaff2872ee2b499b5119e13f92472a7693eb3
Emilie Lewe <emylewe@gmail.com>
2023-10-04 12:13:51 +0100

    TP sur les classes et objets, Création de la classe

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -11,9 +11,9 @@ fun main(args: Array<String>) {
     //   val temperature = 10
     //   val isHot = if (temperature > 50) true else false
     //   println(isHot)
-    val temperature = 10
-    val message = "The water temperature is ${ if( temperature > 50) "too warm" else "OK"}."
-    println(message)
+    //val temperature = 10
+    //val message = "The water temperature is ${ if( temperature > 50) "too warm" else "OK"}."
+    //println(message)
 }
 
 
---
+++ src/main/kotlin/Tp_objet_et_classe/example/myapp/Aquarium.kt
@@ -0,0 +1,13 @@
+package Tp_objet_et_classe.example.myapp
+
+class Aquarium {
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
+
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+    }
+}
