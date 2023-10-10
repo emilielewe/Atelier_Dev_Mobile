7bc5231bf162dbfb8067b2702cd36981bc5fe28d
Emilie Lewe <emylewe@gmail.com>
2023-10-10 22:18:49 +0100

    Réorganisation du projet

--- src/main/kotlin/example/myapp/Main.kt
+++
@@ -1,9 +0,0 @@
-package example.myapp
-
-fun buildAquarium() {
-    val myAquarium = Aquarium()
-}
-
-fun main() {
-    buildAquarium()
-}
---
+++ tp_classe_et_objet/src/main/kotlin/example/myapp/Aquarium.kt
@@ -0,0 +1,21 @@
+package example.myapp
+
+class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    //var width: Int = 20
+    //var height: Int = 40
+    //var length: Int = 100
+
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} liters")
+    }
+
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+    }
+}
