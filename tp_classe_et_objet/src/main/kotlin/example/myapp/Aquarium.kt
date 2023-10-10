aed88fe90c8744c257cf9ec557b093bae82616a0
Emilie Lewe <emylewe@gmail.com>
2023-10-10 22:39:15 +0100

    Ajout des constructeurs de classe; Ajout d'un getter

--- tp_classe_et_objet/src/main/kotlin/example/myapp/Aquarium.kt
+++ tp_classe_et_objet/src/main/kotlin/example/myapp/Aquarium.kt
@@ -6,16 +6,28 @@ class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40
     //var length: Int = 100
 
     init {
-        println("aquarium initializing")
+      println("aquarium initializing")
     }
-    init {
+    //init {
         // 1 liter = 1000 cm^3
-        println("Volume: ${width * length * height / 1000} liters")
+    // println("Volume: ${width * length * height / 1000} liters")
+    //}
+
+    val volume: Int
+        get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
+
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        // calculate the height needed
+        height = (tank / (length * width)).toInt()
     }
 
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
+        // 1 liter = 1000 cm^3
+        println("Volume: $volume liters")
     }
 }
