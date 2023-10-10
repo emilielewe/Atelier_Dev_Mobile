8dfd4e9b8a85a683cf32faafe7195a3ec7fd2503
Emilie Lewe <emylewe@gmail.com>
2023-10-11 00:49:37 +0100

    Création d'une application interactive Dice Roller

--- tp_classe_et_objet/src/main/kotlin/example/myapp/Aquarium.kt
+++ tp_classe_et_objet/src/main/kotlin/example/myapp/Aquarium.kt
@@ -13,8 +13,11 @@ class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40
     // println("Volume: ${width * length * height / 1000} liters")
     //}
 
-    val volume: Int
+    var volume: Int
         get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
 
     constructor(numberOfFish: Int) : this() {
         // 2,000 cm^3 per fish + extra room so water doesn't spill
--- tp_classe_et_objet/src/main/kotlin/example/myapp/Main.kt
+++ tp_classe_et_objet/src/main/kotlin/example/myapp/Main.kt
@@ -23,7 +23,8 @@ fun buildAquarium() {
     val aquarium6 = Aquarium(numberOfFish = 29)
     aquarium6.printSize()
     //println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
-
+    aquarium6.volume = 70
+    aquarium6.printSize()
 }
 
 fun main() {
